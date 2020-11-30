709eadb886f76163cbaa1cc121fd890704e871a0
Андрей Феофанов <voltzwabble@gmail.com>
2020-11-30 11:18:36 +0300

    Добавлен скрипт для таблицы, которая необходима программе

---
+++ Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE Test2.dbo.GOLDatas (
+  ID int IDENTITY,
+  State nvarchar(max) NULL,
+  StateDate datetime NOT NULL DEFAULT (getdate()),
+  CONSTRAINT [PK_dbo.GOLDatas] PRIMARY KEY CLUSTERED (ID)
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
